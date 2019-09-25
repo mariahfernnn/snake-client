85d6a1cfa26641d096e67c3bc7fae5eae042eba9
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-09-25 21:12:00 +0000

    Stdin Setup Assignment

--- play.js
+++ play.js
@@ -1,5 +1,20 @@
-const net = require('net');
-
-const { connect } = require('./client');
+const connect = require('./client');
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  const handleUserInput = function () {
+  stdin.on('data', (key) => {
+    if (key === `\u0003`) {
+      process.exit();
+    }
+  })
+  }
+  stdin.resume();
+  return stdin;
+}
+
+
