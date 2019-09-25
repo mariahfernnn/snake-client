c196e682c9fd9cbdecbd2140b91f4e5ec0ced0d2
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-09-25 22:58:55 +0000

    WASD Movement Challenge

--- input.js
+++ input.js
@@ -1,16 +1,29 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
+
   const handleUserInput = function () {
-  stdin.on('data', (key) => {
-    if (key === `\u0003`) {
-      process.exit();
-    }
-  })
+    stdin.on('data', (key) => {
+      if (key === `\u0003`) {
+        process.exit();
+      } else if (key === 'w') {
+        conn.write('Move: up');
+      } else if (key === 'a') {
+       conn.write('Move: left');
+      } else if (key === 's') {
+        conn.write('Move: down');
+      } else if (key === 'd') {
+        conn.write('Move: right');
+      }
+    })
   }
+  handleUserInput();
   stdin.resume();
   return stdin;
 }
 
-module.exports = setupInput
+module.exports = { setupInput };
