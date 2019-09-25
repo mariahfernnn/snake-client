e34a97274fc1cd6488c0e48a74e6e155d72fe1d5
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-09-25 23:15:33 +0000

    Sending Message Challenge

--- input.js
+++ input.js
@@ -18,6 +18,10 @@ const setupInput = function(conn) {
         conn.write('Move: down');
       } else if (key === 'd') {
         conn.write('Move: right');
+      } else if (key === 'q') {
+        conn.write('Say: YAY');
+      } else if (key === 'e') {
+        conn.write('Say: BYE');
       }
     })
   }
