fd739b415e588fe0b1258764e117d1420f90aef2
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-09-25 19:48:58 +0000

    Snake Initials Assignment

--- client.js
+++ client.js
@@ -2,17 +2,22 @@ const net = require('net');
 
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '172.46.2.204',
-    port: 50541
+    host: '172.46.0.149',
+    port: 50541,
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
   conn.on('data', (data) => {
-    console.log('Server says: ', data);
+    console.log('Server says: You ded cuz you idled');
   });
+  conn.on('connect', (connect) => {
+    console.log('Successfully connected to game server');
+  });
+
+  conn.write('Name: MHF')
 
   return conn;
 }
 
-module.exports = { connect };
+module.exports = connect
