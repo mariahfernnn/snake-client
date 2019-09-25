6f9cf38c2158fbac6c37688b65c0306d22ab63c6
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-09-25 23:24:14 +0000

    Constants Module

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
 const net = require('net');
+const { IP, PORT } = require('./constants');
 
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '172.46.0.149',
-    port: 50541,
+    host: IP,
+    port: PORT,
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
@@ -15,41 +16,8 @@ const connect = function() {
     console.log('Successfully connected to game server');
   });
 
-  // setInterval(() => {conn.write("Move: up")}, 500)
-  // setTimeout(() => {conn.write("Move: right")}, 3000)
-  // setTimeout(() => {conn.write("Move: down")}, 1000)
-  // setTimeout(() => {conn.write("Move: right")}, 1000)
-  // setTimeout(() => {conn.write("Move: left")}, 1000)
- 
-  
-
-  //let delay = key * 1000;
-
-  // conn.on('Move: up', (key) => {
-  //   conn.write('Move: down');
-    // setTimeout(() => {
-    // }, delay);
-  // });
-  
-  // conn.on('Move: down', (key) => {
-  //   conn.write('Move down one square');
-  //   setTimeout(() => {
-  //   }, delay);
-  // });
-
-  // conn.on('Move: left', (key) => {
-  //   conn.write('Move left one square');
-  //   setTimeout(() => {
-  //   }, delay);
-  // });
-
-  // conn.on('Move: right', (key) => {
-  //   conn.write('Move right one square');
-  //   setTimeout(() => {
-  //   }, delay);
-  // });
-
   conn.write('Name: MHF')
+  conn.write('Say: HEY')
 
   return conn;
 
